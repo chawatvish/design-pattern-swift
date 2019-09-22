3a30a87b024ec65db256638338961af416c8829b
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2019-09-22 17:03:03 +0700

    init project with unit test.

--- DesignPatterns/main.swift
+++ DesignPatterns/main.swift
@@ -1,12 +1,3 @@
-//
-//  main.swift
-//  DesignPatterns
-//
-//  Created by Chawatvish Worrapoj on 22/9/2562 BE.
-//  Copyright © 2562 Chawatvish Worrapoj. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
 
---
+++ DesignPatternsUnitTest/DesignPatternsUnitTest.swift
@@ -0,0 +1,33 @@
+//
+//  DesignPatternsUnitTest.swift
+//  DesignPatternsUnitTest
+//
+//  Created by Chawatvish Worrapoj on 22/9/2562 BE.
+//  Copyright © 2562 Chawatvish Worrapoj. All rights reserved.
+//
+
+import XCTest
+
+class DesignPatternsUnitTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
