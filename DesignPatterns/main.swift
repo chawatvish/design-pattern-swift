95462949c6d7876a2a5ca8c71575d0f80ccafafc
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2019-09-22 17:01:00 +0700

    Initial Commit

---
+++ DesignPatterns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DesignPatterns
+//
+//  Created by Chawatvish Worrapoj on 22/9/2562 BE.
+//  Copyright © 2562 Chawatvish Worrapoj. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
