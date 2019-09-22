296e64b8079bc60ef29500c899e258889a5f7678
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2019-09-22 17:22:47 +0700

    clean up

--- DesignPatterns/Factory Method/FactoryMethodTests.swift
+++ DesignPatterns/Factory Method/FactoryMethodTests.swift
@@ -3,15 +3,9 @@ import XCTest
 
 class FactoryMethodTests: XCTestCase {
     func testFactoryMethodRealWorld() {
-
            let info = "Very important info of the presentation"
-
            let clientCode = ClientCode()
-
-           /// Present info over WiFi
            clientCode.present(info: info, with: WifiFactory())
-
-           /// Present info over Bluetooth
            clientCode.present(info: info, with: BluetoothFactory())
        }
 }
--- DesignPatternsUnitTest/Factory Method/FactoryMethod.swift
+++ DesignPatternsUnitTest/Factory Method/FactoryMethod.swift
@@ -68,8 +68,6 @@ class WifiProjector: Projector {
     }
 
     func update(with page: Int) {
-        /// ... scroll page via WiFi connection
-        /// ...
         currentPage = page
     }
 }
@@ -83,8 +81,6 @@ class BluetoothProjector: Projector {
     }
 
     func update(with page: Int) {
-        /// ... scroll page via Bluetooth connection
-        /// ...
         currentPage = page
     }
 }
@@ -95,22 +91,12 @@ class ClientCode {
 
     func present(info: String, with factory: ProjectorFactory) {
 
-        /// Check wheater a client code already present smth...
-
         guard let projector = currentProjector else {
-
-            /// 'currentProjector' variable is nil. Create a new projector and
-            /// start presentation.
-
             let projector = factory.createProjector()
             projector.present(info: info)
             self.currentProjector = projector
             return
         }
-
-        /// Client code already has a projector. Let's sync pages of the old
-        /// projector with a new one.
-
         self.currentProjector = factory.syncedProjector(with: projector)
         self.currentProjector?.present(info: info)
     }
