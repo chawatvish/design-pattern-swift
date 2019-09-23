af2d41c8d1d478b106c9576606b1a217cc7814ff
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2019-09-23 07:03:13 +0700

    clean code

--- DesignPatternsUnitTest/Factory Method/FactoryMethod.swift
+++ DesignPatternsUnitTest/Factory Method/FactoryMethod.swift
@@ -1,5 +1,27 @@
 import Foundation
 
+protocol Projector {
+
+    /// Abstract projector interface
+
+    var currentPage: Int { get }
+
+    func present(info: String)
+
+    func sync(with projector: Projector)
+
+    func update(with page: Int)
+}
+
+extension Projector {
+
+    /// Base implementation of Projector methods
+
+    func sync(with projector: Projector) {
+        projector.update(with: currentPage)
+    }
+}
+
 protocol ProjectorFactory {
 
     func createProjector() -> Projector
@@ -34,28 +56,6 @@ class BluetoothFactory: ProjectorFactory {
 
     func createProjector() -> Projector {
         return BluetoothProjector()
-    }
-}
-
-protocol Projector {
-
-    /// Abstract projector interface
-
-    var currentPage: Int { get }
-
-    func present(info: String)
-
-    func sync(with projector: Projector)
-
-    func update(with page: Int)
-}
-
-extension Projector {
-
-    /// Base implementation of Projector methods
-
-    func sync(with projector: Projector) {
-        projector.update(with: currentPage)
     }
 }
 
